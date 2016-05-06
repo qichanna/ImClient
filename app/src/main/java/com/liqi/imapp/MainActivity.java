4543e8ab31a907b01746ced24a5ac3700f1734c9
liqi7 <liqi7@lenovo.com>
2016-05-06 17:06:01 +0800

    连接到阿里云的ip，加入Handler更新UI

--- app/src/main/java/com/liqi/imapp/MainActivity.java
+++ app/src/main/java/com/liqi/imapp/MainActivity.java
@@ -1,16 +1,15 @@
 package com.liqi.imapp;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.DataInputStream;
@@ -61,9 +60,20 @@ public class MainActivity extends AppCompatActivity {
             }});
 
         new Thread(r).start();
+//        handler.post(r);
     }
 
-    public void connect() {
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == 1){
+                tv.setText(tv.getText() + msg.obj.toString() + '\n');//显示到显示框中
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    /*public void connect() {
         try {
             s = new Socket("192.168.1.3", 8888); //建立客户端对象
             dos = new DataOutputStream(s.getOutputStream());
@@ -75,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             Log.e("liqix", e.getMessage());
         }
-    }
+    }*/
 
     public void disconnect() { //窗口关闭时关闭客户端，输入，输出
         try {
@@ -97,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             try {
                 s = new Socket(); //建立客户端对象
-                s.connect(new InetSocketAddress("192.168.1.3", 8888), 5000);
+                s.connect(new InetSocketAddress("115.28.213.111", 8888), 5000);
                 dos = new DataOutputStream(s.getOutputStream());
                 dis = new DataInputStream(s.getInputStream());
                 bConnected = true;
@@ -111,6 +121,10 @@ public class MainActivity extends AppCompatActivity {
                 while (bConnected) {
                     String str;
                     str = dis.readUTF(); //拿到数据
+                    Message m = new Message();
+                    m.what = 1;
+                    m.obj = str;
+                    handler.sendMessage(m);
 //                    tv.setText(tv.getText() + str + '\n');//显示到显示框中
                 }
             } catch (SocketException e) {
